8249252fe38750cc02a926685f44546ab28d39ac
Nicoclock <nicolas.charpin@oclock.io>
2021-11-04 08:19:56 +0000

    Correction async await

--- app/controllers/tagController.js
+++ app/controllers/tagController.js
@@ -1,41 +1,84 @@
 const {Tag} = require('../models');
 
 module.exports = {
-    tagsPage: async (request, response) => {
+    // tagsPage: async (request, response) => {
 
-        Tag.findAll()
-        .then( (tags) => {
+    //     Tag.findAll()
+    //     .then( (tags) => {
+    //         response.render('tags', {tags});
+    //     }).catch( (error) => {
+    //         response.status(500).send(error.message);
+    //     });
+    // },
+
+    tagsPage: async (request, response) => {
+        try {
+            const tags = await Tag.findAll();
             response.render('tags', {tags});
-        }).catch( (error) => {
+        } catch (error) {
             response.status(500).send(error.message);
-        });
+        }
     },
+
+
+
+    // quizzesByTag: async (request, response) => {
+
+    //     // on récupère l'id dans les paramètres d'url
+    //     const tagId = request.params.id;
+
+    //     // on va chercher le tag correspondant
+    //     // on demande le Tag, en incluant les quizzes qui sont associés, ET pour chacun de ces quizzes, en incluant aussi l'auteur
+    //     Tag.findByPk(tagId, {
+    //         include: [
+    //             {
+    //                 association: "quizzes",
+    //                 include: [
+    //                     {association: "author"}
+    //                 ]
+    //             }
+    //         ]
+    //     }).then( (tag) => {
+    //         // si il existe => render
+    //         if (tag) {
+
+    //             response.render('tag', {tag});
+    //         } else {
+    //             // sinon => 404
+    //             response.status(500).send(`Aucun tag d\id ${tagId} en BDD`)
+    //         }
+    //     });
+    // }    
 
     quizzesByTag: async (request, response) => {
 
         // on récupère l'id dans les paramètres d'url
-        const tagId = request.params.id;
+        const tagId = +request.params.id;
 
         // on va chercher le tag correspondant
         // on demande le Tag, en incluant les quizzes qui sont associés, ET pour chacun de ces quizzes, en incluant aussi l'auteur
-        Tag.findByPk(tagId, {
-            include: [
-                {
-                    association: "quizzes",
-                    include: [
-                        {association: "author"}
-                    ]
-                }
-            ]
-        }).then( (tag) => {
+        
+        try {
+            const tag = await Tag.findByPk(tagId, {
+                include: [
+                    {
+                        association: "quizzes",
+                        include: [
+                            {association: "author"}
+                        ]
+                    }
+                ]
+            });
             // si il existe => render
             if (tag) {
-
+    
                 response.render('tag', {tag});
             } else {
                 // sinon => 404
                 response.status(500).send(`Aucun tag d\id ${tagId} en BDD`)
             }
-        });
-    }    
+        } catch (error) {
+            response.status(500).send(error.message);
+        }
+    }
 }
