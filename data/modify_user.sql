2e55db98d592e1211c86112df90f94302f420091
Oscar Lienard <lienard.oscar@gmail.com>
2021-11-08 22:13:53 +0100

    maj sql creation table

--- data/modify_user.sql
+++ data/modify_user.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
-ALTER TABLE "user"
-    ADD COLUMN "role" TEXT DEFAULT 'member';
+ALTER TABLE "user" ADD COLUMN "role" TEXT DEFAULT 'member';
+
+UPDATE "user" SET "role" = 'admin' WHERE "email" = 'o@l.com';
 
 COMMIT;
