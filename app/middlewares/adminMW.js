2d4cb154cfa928a3ee00ea77585880c7bb68e7e9
Oscar Lienard <lienard.oscar@gmail.com>
2021-11-08 21:21:56 +0100

    first commit

--- app/middlewares/adminMW.js
+++ app/middlewares/adminMW.js
@@ -1,10 +1,12 @@
 module.exports = (request, response, next) => {
     if (!request.session.user) {
+        console.log("yo1");
         //user non connecté, on le redirige sur la page login
         response.redirect('/login');
     } else {
         //le user est connecté
         if (request.session.user.role === 'admin') {
+            console.log("yo2");
             next();
         } else {
             response.render('401');
